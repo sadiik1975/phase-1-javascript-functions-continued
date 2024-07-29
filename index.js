79887632b570c91bcdb6709b7cd81a239a5573d9
Sadeek Been <sadiikbeen@gmail.com>
2024-07-29 14:47:17 -0400

    adding update

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// index.js
+
+// Define the saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function expression
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+  }
+  
+  // Export the functions for testing
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
